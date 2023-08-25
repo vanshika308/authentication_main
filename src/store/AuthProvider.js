02b410755660a258723f4abccd84bc12b73460e0
vanshika308 <vanshikatiwari0308@gmail.com>
2023-08-26 04:23:05 +0530

    auto logout

--- src/store/AuthProvider.js
+++ src/store/AuthProvider.js
@@ -1,40 +1,52 @@
-import {  useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import AuthContext from "./auth-context";
 
-const AuthProvider=(props)=>{
-    const storedToken = localStorage.getItem("token");
-    const[token,setToken] = useState(null);
-    const userIsLoggedIn = !!token;
+const AuthProvider = (props) => {
+  const storedToken = localStorage.getItem("token");
+  const [token, setToken] = useState(storedToken);
+  const userIsLoggedIn = !!token;
 
-    const login=(token)=>{
-        setToken(token);
-        localStorage.setItem("token", token);
-        console.log(token);
-    };
+  const login = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+    setAutoLogout();
+  };
 
-    const logout =()=>{
-        setToken(null);
-        localStorage.removeItem("token");
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    clearAutoLogout();
+  };
+
+  const setAutoLogout = () => {
+    const autoLogoutTime = 5 * 60 * 1000; // 5 minutes in milliseconds
+    setTimeout(() => {
+      logout();
+    }, autoLogoutTime);
+  };
+
+  const clearAutoLogout = () => {
+    clearTimeout();
+  };
+
+  useEffect(() => {
+    if (token) {
+      setAutoLogout();
     }
-    const authContextValue = {
-        token: token,
-        login: login,
-        logout: logout,
-        isLoggedIn: userIsLoggedIn
-    };
+  }, [token]);
 
-    useEffect(() => {
-        if (storedToken) {
-          setToken(storedToken);
-        }
-      }, []);
-    
+  const authContextValue = {
+    token: token,
+    login: login,
+    logout: logout,
+    isLoggedIn: userIsLoggedIn,
+  };
 
-    return(
-        <AuthContext.Provider value={authContextValue}>
-            {props.children}
-        </AuthContext.Provider>
-    );
-}
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthProvider;
